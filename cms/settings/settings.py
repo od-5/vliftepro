17bc231257822a63fe3b9bc54f00283b30400001
Alexey <od-5@yandex.ru>
2015-07-21 01:00:35 +0300

    fix

--- cms/settings/settings.py
+++ cms/settings/settings.py
@@ -58,10 +58,21 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vliftepro',
+        'USER': 'vliftepro',
+        'PASSWORD': 'f3ccb431',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
