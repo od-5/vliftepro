c43b61957082ef64a62d9586eedf8ec413b44371
alexy <od-5@yandex.ru>
2015-07-22 01:37:10 +0300

    fix

--- cms/settings/settings.py
+++ cms/settings/settings.py
@@ -58,23 +58,23 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'vliftepro',
-#         'USER': 'vliftepro',
-#         'PASSWORD': 'f3ccb431',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'vliftepro',
+         'USER': 'vliftepro',
+         'PASSWORD': 'f3ccb431',
+         'HOST': 'localhost',
+         'PORT': '',
+     }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
